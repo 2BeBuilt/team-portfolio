1c7994ab2795e64067a33925fb090a78e0732362
Andrey Gruzdev <agruzdev1@gmail.com>
2023-09-03 21:27:44 +0200

    update default theme

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,13 +1,14 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
 
-import theme from '@/utils/theme'
-import Fonts from '@/utils/fonts'
 import Layout from '@/components/Layout/Layout'
+import Fonts from '@/utils/fonts'
+import theme from '@/utils/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={'dark'} />
       <Fonts />
       <Layout>
         <Component {...pageProps} />
