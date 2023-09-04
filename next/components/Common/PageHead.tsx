9906ed8e3c97efd03d7c5a5fdcd59079121772a5
Andrey Gruzdev <agruzdev1@gmail.com>
2023-09-04 12:03:31 +0200

    optimization

--- next/components/Common/PageHead.tsx
+++ next/components/Common/PageHead.tsx
@@ -7,8 +7,10 @@ interface PageHeadProps {
 export default function PageHead(props: PageHeadProps) {
   return (
     <Head>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <title>{props.title}</title>
       <meta property={'og:title'} content={'We be buildin'} key={'title'} />
+      <meta name={'description'} content={'2BeBuilt - we be buildin'} />
     </Head>
   )
 }
