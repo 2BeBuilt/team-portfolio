98ed704d1581578e5e6c7c53a4708d5d70bfa011
Andrey Gruzdev <agruzdev1@gmail.com>
2023-09-09 14:07:43 +0200

    remove unused

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -2,13 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout/Layout'
-import Fonts from '@/utils/fonts'
 import theme from '@/utils/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Fonts />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- next/pages/_document.tsx
+++ next/pages/_document.tsx
@@ -1,14 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
-
-import theme from '@/utils/theme'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <ColorModeScript initialColorMode={theme.initialColorMode} />
         <Main />
         <NextScript />
       </body>
