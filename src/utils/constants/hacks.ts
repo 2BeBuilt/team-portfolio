de4146f74266e8d2bfd98346bb017172c95ab8dc
Andrey <canvi@2bb.dev>
2025-10-18 15:38:42 +0200

    chore: migrate hackathon and sponsor logos to local assets

--- src/utils/constants/hacks.ts
+++ src/utils/constants/hacks.ts
@@ -3,7 +3,7 @@ import Hackathon from '@/interfaces/hack.dto'
 const hacks: Hackathon[] = [
   {
     name: 'ETHGlobal Paris 2023',
-    logo: 'https://storage.googleapis.com/ethglobal-api-production/events%2Fzqd1s%2Flogo%2F1683191744558_paris_2023_square_logo.png',
+    logo: '/ethglobal-paris-2023.png',
     date: 'July 21 – 23, 2023',
     project: 'SourceScan.BOS',
     prizes: [
@@ -11,14 +11,14 @@ const hacks: Hackathon[] = [
         nomination: '"🥈 Best Frontend built with BOS"',
         sponsor: {
           name: 'NEAR',
-          logo: 'https://storage.googleapis.com/ethglobal-api-production/organizations%2F5b69q%2Flogo%2F1666293405537_YPwdd6cd_400x400.jpeg',
+          logo: '/near-logo.jpeg',
         },
       },
     ],
   },
   {
     name: 'Harvard Blockchain x EasyA Hackathon',
-    logo: 'https://ipfs.io/ipfs/bafkreign4u2iurpsdrjehbvsar7fl6ofbb3p4x5r7olswnqpo3se3vsukm',
+    logo: '/harvard-blockchain.jpg',
     date: '31 March - 2 April 2023',
     project: 'Warp Drive',
     prizes: [
@@ -26,14 +26,14 @@ const hacks: Hackathon[] = [
         nomination: '"🥇 1st Place"',
         sponsor: {
           name: 'Axelar',
-          logo: 'https://avatars.githubusercontent.com/u/67522309?s=280&v=4',
+          logo: '/axelar-logo.jpg',
         },
       },
     ],
   },
   {
     name: 'ETHGlobal Scaling Ethereum 2023',
-    logo: 'https://storage.googleapis.com/ethglobal-api-production/events%2Fof2r2%2Flogo%2F1671503482706_scaling-ethereum-square-logo.svg',
+    logo: '/ethglobal-scaling-ethereum-2023.svg',
     date: '10 - 31 March 2023',
     project: 'TimePact',
     prizes: [
@@ -41,14 +41,14 @@ const hacks: Hackathon[] = [
         nomination: '"Best Primitives or Infra"',
         sponsor: {
           name: 'FVM',
-          logo: 'https://storage.googleapis.com/ethglobal-api-production/organizations%2F9zj01%2Flogo%2F1677081097389_FVM%20square%20logo.png',
+          logo: '/fvm-logo.png',
         },
       },
       {
         nomination: '"Just Deploy!"',
         sponsor: {
           name: 'Scroll',
-          logo: 'https://storage.googleapis.com/ethglobal-api-production/organizations%2Fyip67%2Fimages%2FETH%20Tokyo%202023%20_logo_400_400%402x.png',
+          logo: '/scroll-logo.png',
         },
       },
     ],
