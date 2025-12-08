57b1e579163383c35fbf2b1f9e0737477f875130
Andrey <canvi@2bb.dev>
2025-12-08 13:47:54 +0100

    feat: add favicon and PWA manifest
    - Add new favicon.ico, SVG icon, and apple-touch-icon
    - Add web app manifest with 192x192 and 512x512 icons
    - Update PageHead with proper meta tags

--- src/components/Common/PageHead.tsx
+++ src/components/Common/PageHead.tsx
@@ -7,10 +7,14 @@ interface PageHeadProps {
 export default function PageHead(props: PageHeadProps) {
   return (
     <Head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <title>{props.title}</title>
-      <meta property={'og:title'} content={'We be buildin'} key={'title'} />
-      <meta name={'description'} content={'2BeBuilt - we be buildin'} />
+      <meta property="og:title" content="We be buildin" key="title" />
+      <meta name="description" content="2BeBuilt - we be buildin" />
+      <meta name="apple-mobile-web-app-title" content="2bb" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon0.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   )
 }
